baea4c214f13d9ff34f0154b0ee495e0028b0aba
eric.brewer <eric.brewer@sandboxww.com>
2016-09-09 14:06:31 -0400

    made navigation and homepage

--- client/app/app.js
+++ client/app/app.js
@@ -1,20 +1,44 @@
 import angular from 'angular';
 import uiRouter from 'angular-ui-router';
-import Common from './common/common';
-import Components from './components/components';
 import AppComponent from './app.component';
+import NavigationComponent from './components/navigation/navigation';
+
+//default styles
 import 'normalize.css';
+import 'bootstrap/dist/css/bootstrap.css';
 
 angular.module('app', [
-    uiRouter,
-    Common.name,
-    Components.name
-  ])
-  .config(($locationProvider) => {
-    "ngInject";
-    // @see: https://github.com/angular-ui/ui-router/wiki/Frequently-Asked-Questions
-    // #how-to-configure-your-server-to-work-with-html5mode
-    $locationProvider.html5Mode(true).hashPrefix('!');
-  })
+  uiRouter,
 
-  .component('app', AppComponent);
+  //inject the .name property of our import
+  NavigationComponent.name
+])
+
+.config(($locationProvider, $stateProvider, $urlRouterProvider)=>{
+  "ngInject";
+
+  //define app routing.
+  //main route is abstract and cannont be viewed without children.
+  $stateProvider
+    .state('app', {
+      url:'/app',
+      abstract: true,
+      template: "<app></app>"
+    })
+
+    //dashboard page
+    .state('app.home', {
+      url:'/home',
+      template: 'Home page'
+    })
+
+    //route for goat listing creator
+    .state('app.create', {
+      url:"/create",
+      template: 'Create Page'
+    });
+
+    //default state
+    $urlRouterProvider.otherwise('/app/home');
+})
+.component('app', AppComponent);
